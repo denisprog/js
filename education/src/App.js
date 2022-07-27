669a4b933d14e0a400f924bbf624f8777a081958
Denis <denisprog2009@gmail.com>
2022-07-27 19:43:25 +0300

    added hello

--- education/src/App.js
+++ education/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
+      <h1>HELLO</h1>
       <button onClick={setLikes}>Increase</button>
       <button onclick={setLikes}>Decrease</button>
     </div>
