7c193328077f215530f93fddc83976712160ece2
Dzianis <denisprog2009@gmail.com>
2022-07-27 15:35:34 +0300

    init commit

---
+++ education/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={setLikes}>Increase</button>
+      <button onclick={setLikes}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
