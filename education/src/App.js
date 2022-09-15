3e8ddef43419447e0fcf82a9ed9b68490c188203
Denis <denisprog2009@gmail.com>
2022-09-16 00:12:26 +0300

    37

--- education/src/App.js
+++ education/src/App.js
@@ -3,11 +3,20 @@ import FunctionalComponent from "./FunctionalComponent";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
+    const expenses = [
+        {expenseDate:new Date(2022, 2, 22), expenseTitle:"Car Insurancee", expenseAmount:256},
+        {expenseDate:new Date(2022, 3, 23), expenseTitle:"Milk",           expenseAmount:1.5}
+    ];
     return(
         <div>
             <ClassComponent name="Denis"/>
             <FunctionalComponent name="Denis"/>
-            <ExpenseItem />
+
+            {
+                expenses.map(expense =>{
+                    return <ExpenseItem {...expense} />;
+                })
+            };
         </div>
     );
 }
--- education/src/components/ExpenseItem.js
+++ education/src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  console.log(props);
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.expenseTitle}</h2>
+        <div className="expense-item__price">${props.expenseAmount}</div>
       </div>
     </div>
   );
