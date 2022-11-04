fef704b5a448e0a08659d801ea8541748095fd48
DLiseichykau <dliseichykau@redcapcloud.com>
2022-11-04 15:16:35 +0300

    38-39

--- education/src/components/ExpenseItem.js
+++ education/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
   console.log(props);
 
+
+
   return (
     <div className="expense-item">
-      <div>{props.expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.expenseDate} />
       <div className="expense-item__description">
         <h2>{props.expenseTitle}</h2>
         <div className="expense-item__price">${props.expenseAmount}</div>
