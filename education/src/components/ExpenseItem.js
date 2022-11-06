d1447e13843ebfb4c0d4a0f237f3947d9e29e6d8
Denis <denisprog2009@gmail.com>
2022-11-07 00:07:07 +0300

    40

--- education/src/components/ExpenseItem.js
+++ education/src/components/ExpenseItem.js
@@ -1,19 +1,18 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import Card from './Card'
 
 function ExpenseItem(props) {
   console.log(props);
 
-
-
   return (
-    <div className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.expenseDate} />
       <div className="expense-item__description">
         <h2>{props.expenseTitle}</h2>
         <div className="expense-item__price">${props.expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- education/src/components/Expenses.js
+++ education/src/components/Expenses.js
@@ -1,15 +1,16 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {
                 props.expenses.map(expense =>{
                     return <ExpenseItem {...expense} />;
                 })
             }
-        </div>
+        </Card>
     )
 }
 
