fe1f585130284f97d2faca049f8e3e87dfa9ad0a
DLiseichykau <dliseichykau@redcapcloud.com>
2022-11-09 11:22:17 +0300

    simple: changed field names

--- education/src/App.js
+++ education/src/App.js
@@ -5,8 +5,8 @@ import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
     const expenses = [
-        {expenseDate:new Date(2022, 2, 22), expenseTitle:"Car Insurancee", expenseAmount:256},
-        {expenseDate:new Date(2022, 3, 23), expenseTitle:"Milk",           expenseAmount:1.5}
+        {date:new Date(2022, 2, 22), title:"Car Insurancee", amount:256},
+        {date:new Date(2022, 3, 23), title:"Milk",           amount:1.5}
     ];
     return(
         <div>
--- education/src/components/Expenses/ExpenseItem.js
+++ education/src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.expenseTitle);
+  const [title, setTitle] = useState(props.title);
   console.log('Expense Item evaluated by React');
 
 
@@ -17,10 +17,10 @@ const ExpenseItem = (props) => {
 
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={props.expenseDate} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${props.expenseAmount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
     </Card>
