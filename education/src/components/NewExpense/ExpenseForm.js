dec36da148d1ad5c8ee60a3dc6112c23e5197f2b
DLiseichykau <dliseichykau@redcapcloud.com>
2022-11-09 10:56:34 +0300

    58

--- education/src/components/NewExpense/ExpenseForm.js
+++ education/src/components/NewExpense/ExpenseForm.js
@@ -56,7 +56,10 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
-        console.log(expenseData)
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
 
@@ -66,15 +69,24 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input 
+                        type="text" 
+                        onChange={titleChangeHandler} 
+                        value={enteredTitle}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input 
+                        type="number" min="0.01" step="0.01" 
+                        onChange={amountChangeHandler}
+                        value={enteredAmount}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input 
+                        type="date" min="2019-01-01" max="2022-12-31" 
+                        onChange={dateChangeHandler}
+                        value={enteredDate}/>
                 </div>
             </div>
             <div className="new-expense__actions">
