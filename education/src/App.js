bf93d2ad70eafb8e83582f08d4ef3c8bf5611573
Denis <denisprog2009@gmail.com>
2022-11-13 15:43:41 +0300

    65

--- education/src/App.js
+++ education/src/App.js
@@ -2,18 +2,26 @@ import ClassComponent from "./ClassComponent";
 import FunctionalComponent from "./FunctionalComponent";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+    {id:"1", date:new Date(2019, 2, 22), title:"Potato", amount:256},
+    {id:"2", date:new Date(2020, 3, 23), title:"Milk",           amount:150},
+    {id:"3", date:new Date(2021, 3, 23), title:"Butter",           amount:100},
+    {id:"4", date:new Date(2022, 3, 23), title:"Bread",           amount:200},
+];
 
 const App = () => {
-    const expenses = [
-        {id:"1", date:new Date(2019, 2, 22), title:"Potato", amount:256},
-        {id:"2", date:new Date(2020, 3, 23), title:"Milk",           amount:150},
-        {id:"3", date:new Date(2021, 3, 23), title:"Butter",           amount:100},
-        {id:"4", date:new Date(2022, 3, 23), title:"Bread",           amount:200},
-    ];
+
+
+    const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = expense =>{
-        console.log('In App.js');
-        console.log(expense);
+        //console.log('In App.js');
+        //console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     }
 
     return(
