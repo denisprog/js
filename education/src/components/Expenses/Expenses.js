44290080d2ec821ac5967d852ab455436cd1f3e9
Denis <denisprog2009@gmail.com>
2022-11-13 17:57:05 +0300

    68

--- education/src/components/Expenses/Expenses.js
+++ education/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from 'react'
+import ExpensesList from './ExpensesList';
 
 
 const Expenses = (props) => {
@@ -17,21 +17,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear || filteredYear==='';
     });
 
-    let expensesContent = <p>No expenses found</p>;
-    if (filteredExpenses.length >0){
-        expensesContent = filteredExpenses.map(expense =>{
-            return <ExpenseItem 
-                        key={expense.id} 
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}/>;
-        })
-    }
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeFilter} />
-            { expensesContent }
+            <ExpensesList items={filteredExpenses}/>
 
         </Card>
     )
