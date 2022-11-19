ffa260c98a22409fbe538d76c27b6064f16ca01f
Denis <denisprog2009@gmail.com>
2022-11-19 22:57:28 +0300

    8section: 94-95

--- section08-user-app/src/components/User/AddUser.js
+++ section08-user-app/src/components/User/AddUser.js
@@ -18,6 +18,7 @@ const AddUser = props => {
             return;
         }
         console.log(enteredUsername, enteredAge);
+        props.onAddUser(enteredUsername, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
     };
