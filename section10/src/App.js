6452e8b0d72485429c38ac3f426f21a8dcd7352e
Denis <denisprog2009@gmail.com>
2022-11-22 22:24:36 +0300

    10section-useEffect-110

--- section10/src/App.js
+++ section10/src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
-function App() {
+const IS_LOGGED_KEY = 'isLoggedIn';
+
+function App () {
+
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem(IS_LOGGED_KEY);
+    if(storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);
+  
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem(IS_LOGGED_KEY, '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem(IS_LOGGED_KEY);
     setIsLoggedIn(false);
   };
 
