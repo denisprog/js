842fce6d4eba99281a5c24903eea79b61b6c7893
Denis <denisprog2009@gmail.com>
2022-11-30 21:59:46 +0300

    section10: 124-125

--- section10/src/components/Login/Login.js
+++ section10/src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
+
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT'){
@@ -47,6 +49,8 @@ const Login = (props) => {
 
   const[emailState, dispatchEmail] = useReducer(emailReducer, INIT_EMAIL_STATE);
   const[passwordState, dispatchPassword] = useReducer(passwordReducer, INIT_PASSWORD_STATE);
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -98,7 +102,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.passwordValue);
+    authCtx.onLogin(emailState.value, passwordState.passwordValue);
   };
 
   return (
